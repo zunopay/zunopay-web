4a27d1372855ca9e60f3b7dcac6fc820d3b5aeca
atharmohammad <mohd.rule123@gmail.com>
2025-04-16 15:34:17 +0530

    feat: add ui select component

---
+++ src/components/ui/Select.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { CheckCircleIcon, ArrowDownIcon, ArrowUpIcon } from "lucide-react";
+import { cva, VariantProps } from "class-variance-authority";
+
+const labelVariants = cva('text-base text-white')
+
+export const Select = React.forwardRef<
+  React.ComponentRef<typeof SelectPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Root> & VariantProps<typeof labelVariants>
+>(({ children, ...props }, forwardedRef) => {
+  return (
+    <SelectPrimitive.Root {...props}>
+      <SelectPrimitive.Trigger ref={forwardedRef} className="flex p-1 justify-between bg-white text-black rounded-lg">
+        <SelectPrimitive.Value />
+        <SelectPrimitive.Icon>
+          <ArrowDownIcon />
+        </SelectPrimitive.Icon>
+      </SelectPrimitive.Trigger>
+      <SelectPrimitive.Portal>
+        <SelectPrimitive.Content className="p-1 overflow-hidden text-black rounded-md bg-white shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+          <SelectPrimitive.ScrollUpButton>
+            <ArrowUpIcon />
+          </SelectPrimitive.ScrollUpButton>
+          <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+          <SelectPrimitive.ScrollDownButton>
+            <ArrowDownIcon />
+          </SelectPrimitive.ScrollDownButton>
+        </SelectPrimitive.Content>
+      </SelectPrimitive.Portal>
+    </SelectPrimitive.Root>
+  );
+});
+
+export const SelectItem = React.forwardRef<
+  React.ComponentRef<typeof SelectPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ children, ...props }, forwardedRef) => {
+  return (
+    <SelectPrimitive.Item {...props} ref={forwardedRef} className="flex justify-between px-4 py-1 cursor-pointer">
+      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+      <SelectPrimitive.ItemIndicator>
+        <CheckCircleIcon />
+      </SelectPrimitive.ItemIndicator>
+    </SelectPrimitive.Item>
+  );
+});
