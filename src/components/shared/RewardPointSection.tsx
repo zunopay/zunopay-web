1c7c1ff3d9e7a9e18f4d16abee1846abd7ad18ae
atharmohammad <mohd.rule123@gmail.com>
2025-05-05 10:17:13 +0530

    chore: use real data for reward points

--- src/components/shared/RewardPointSection.tsx
+++ src/components/shared/RewardPointSection.tsx
@@ -1,12 +1,17 @@
+'use client'
+
+import { useFetchRewardPoints } from '@/api/user/queries';
 import React from 'react';
 
 export const RewardPointSection = () => {
+  const {data: reward} = useFetchRewardPoints();
+
   return (
     <div className="bg-primary-100 dark:bg-primary-800 text-primary-800 dark:text-primary-100 px-3 py-1 rounded-full flex items-center">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
       </svg>
-      <span className="text-sm font-medium">1,250 points</span>
+      <span className="text-sm font-medium">{reward?.data || 0} points</span>
     </div>
   );
 };
