90cf03da38598071e4e088b66b2d97be4a81905c
atharmohammad <mohd.rule123@gmail.com>
2025-05-05 15:29:29 +0530

    chore: remove transfer card from dashboard home

--- src/components/wallet/WalletCard.tsx
+++ src/components/wallet/WalletCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { BalanceCard } from "../shared/cards/BalanceCard";
 import { RevenueCard } from "../shared/cards/RevenueCard";
-import { TransferCard } from "../shared/cards/TransferCard";
 
 export default function WalletCard() {
   return (
@@ -9,9 +8,6 @@ export default function WalletCard() {
       <BalanceCard />
       <RevenueCard title="Total Revenue" />
       <RevenueCard title="Daily Revenue" />
-      <div className="col-span-1 md:col-span-2 lg:col-span-3">
-        <TransferCard />
-      </div>
     </section>
   );
 }
