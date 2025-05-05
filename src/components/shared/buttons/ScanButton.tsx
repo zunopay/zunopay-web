c1a84300300530b67ddcd0121b329e908e4f9c62
atharmohammad <mohd.rule123@gmail.com>
2025-05-05 10:05:11 +0530

    feat: improve mobile nav and add links

--- src/components/shared/buttons/ScanButton.tsx
+++ src/components/shared/buttons/ScanButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { EnhancedQrScanner } from "@/components/dialogs/EnchanceQrScanner";
+import { ScannerIcon } from "@/components/icons/theme/ScannerIcon";
+import { Button } from "@/components/ui";
 import React, { useState } from "react";
 
 export const ScanButton: React.FC = () => {
@@ -8,28 +10,16 @@ export const ScanButton: React.FC = () => {
 
   return (
     <>
-      <button
+      <Button
+        variant="ghost"
         onClick={() => toggleScanner(!showScanner)}
-        className="flex flex-col items-center justify-center -mt-8"
+        className="flex flex-col items-center p-2"
       >
         <div className="bg-blue-zunopay p-4 rounded-full shadow-lg mb-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-7 w-7 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1v-2a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1z"
-            />
-          </svg>
+          <ScannerIcon className="text-white" />
         </div>
         <span className="text-xs font-medium text-blue-zunopay">Scan</span>
-      </button>
+      </Button>
       <EnhancedQrScanner
         onClose={() => toggleScanner(!showScanner)}
         open={showScanner}
