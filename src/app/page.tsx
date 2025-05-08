8c0865efa4b4ee3c1640dedad522401cafe0a1b7
atharmohammad <mohd.rule123@gmail.com>
2025-05-08 18:51:59 +0530

    fix: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 import { BackgroundWithNameLayout } from "@/components/layout/BackgroundWithNameLayout";
 import Image from "next/image";
 import SHORT_LOGO from "../../public/Images/short-logo.png";
-import { SolutionSection } from "@/components/Explainer";
-import { TestimonialsSection } from "@/components/Testimonials";
+// import { SolutionSection } from "@/components/Explainer";
+// import { TestimonialsSection } from "@/components/Testimonials";
 import { CTASection } from "@/components/CTASection";
 
 export default function HomePage() {
   return (
-    <BackgroundWithNameLayout showFooter={true} >
-      <div className="flex flex-col md:gap-20 xs:gap-10">
-        <div className="flex justify-center w-full">
-          <Image src={SHORT_LOGO} alt="zunopay" />
-        </div>
-
+    <BackgroundWithNameLayout showFooter={true}>
+      <div className="flex justify-center w-full">
+        <Image src={SHORT_LOGO} alt="zunopay" height={200} width={200}/>
+      </div>
+      <div className="flex flex-col xs:gap-10">
         <CTASection />
-        <SolutionSection />
+        {/* <SolutionSection /> */}
         {/* <TestimonialsSection /> */}
       </div>
     </BackgroundWithNameLayout>
