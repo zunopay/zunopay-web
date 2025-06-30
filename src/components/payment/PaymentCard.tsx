ee352aa377ddad9ba13dcad037172fd35d06f98f
atharmohammad <mohd.rule123@gmail.com>
2025-06-30 12:34:44 +0530

    feat: add payment card

--- src/components/payment/PaymentCard.tsx
+++ src/components/payment/PaymentCard.tsx
@@ -1,12 +1,23 @@
-'use client'
+"use client";
 
-import { ConnectWalletButton } from "../wallet/ConnectWalletButton"
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { ConnectWalletButton } from "../wallet/ConnectWalletButton";
 
-export const PaymentCard : React.FC = () => {
-    return (
-        <div>
-            <p>Pay</p>
-            <ConnectWalletButton />
+export const PaymentCard: React.FC = () => {
+  return (
+    <Card className="max-w-[600px] rounded-2xl">
+      <CardHeader>Pay with USDC</CardHeader>
+      <CardContent className="flex flex-col gap-5">
+        <div className="bg-grey-200 text-white rounded-2xl p-5">
+          Total price: <strong>$10 USDC</strong>
         </div>
-    )
-}
+
+        <ConnectWalletButton className="w-full" />
+        <p className="p-2 rounded-xl brightness-110">
+          Disclaimer: This is a DeFi payment that can't be reversed. Funds go
+          directly to the merchant. See our terms.
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
